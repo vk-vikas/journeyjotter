43c5b1879f6ab95d4d3f1bd31cea943ec1c38f20
vikas kumar <vk.vikas3003@gmail.com>
2025-01-18 09:55:21 +0530

    add classnames and hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Log your perfect travel itineraries and stories
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Share perfect trvale guides for the world to read !
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default page;
